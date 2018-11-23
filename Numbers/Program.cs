32f5d5d44babc2f212927502b66f6392b2b5352e
Pallavi Eraboina <M1026395@mindtree.com>
2018-11-23 16:29:53 +0530

    Creating the repository

---
+++ Numbers/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumWords
+{
+    class Program
+    {
+        
+        static String NumWords(int n) 
+        {
+            
+            string[] numbersArr = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] tensArr = new string[] { "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };         
+            string words = "";
+
+            bool tens = false;
+
+            if (n < 0)
+            {
+                words += "negative ";
+                n *= -1;
+            }
+           
+           
+            if (0 <= n && n <= 999)
+            {
+                if ((int)n / 100 > 0)
+                {
+                    words += NumWords(n / 100) + " hundred";
+                    n %= 100;
+                }
+                if ((int)n / 10 > 1)
+                {
+                    if (words != "")
+                        words += " ";
+                    words += tensArr[(int)n / 10 - 2];
+                    tens = true;
+                    n %= 10;
+                }
+
+                if (n < 20 && n > 0)
+                {
+                    if (words != "" && tens == false)
+                        words += " ";
+                    words += (tens ? "-" + numbersArr[(int)n - 1] : numbersArr[(int)n - 1]);
+                    
+                }
+            }
+
+            return words;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number less than 1000: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}", NumWords(n));
+            Console.ReadLine();
+        }
+    }
+}
